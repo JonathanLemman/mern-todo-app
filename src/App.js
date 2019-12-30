d8e6e6df73d8c8fb581a050a0f1d93a09404521b
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-12-30 12:23:32 -0600

    added styling to app.js and imported components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo";
+import EditTodo from "./components/edit-todo";
+import TodosList from "./components/todos-list";
+
+import logo from "./logo.svg";
 
 
 class App extends Component {
@@ -8,13 +15,28 @@ class App extends Component {
     return (
         <Router>
             <div className="container">
-              <h2>MERN-Stack Todo App</h2>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                        <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com"/>
+                    </a>
+                    <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+                    <div className="collpase navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">Todos</Link>
+                            </li>
+                            <li className="navbar-item">
+                                <Link to="/create" className="nav-link">Create Todo</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <br/>
+                <Route path="/" exact component={TodosList} />
+                <Route path="/edit/:id" component={EditTodo} />
+                <Route path="/create" component={CreateTodo} />
             </div>
-            <Route path="/" exact component={TodosList} />
-            <Route path="/edit/:id" component={EditTodo} />
-            <Route path="/create" component={CreateTodo} />
         </Router>
-
     );
   }
 }
